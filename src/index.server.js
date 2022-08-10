f96c35616824b2a6b9caaf715f069de3c99c15bd
aR <abenezerkifle000@gmail.com>
2022-08-10 19:20:03 +0300

    connected to database

--- src/index.server.js
+++ src/index.server.js
@@ -1,10 +1,20 @@
 const express = require('express');
-const env = require('dotenv')
+const env = require('dotenv');
 const app = express();
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // environemt variable 
 env.config();
+// mongodb connection
+// 
+mongoose.connect(
+    `mongodb+srv://root:root@cluster0.htqxtzb.mongodb.net/?retryWrites=true&w=majority`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
+    console.log('Database Connected');
+});
 // instead pog app,use(express.json())
 app.use(bodyParser());
 
@@ -13,13 +23,13 @@ app.get('/', (req, res, next) => {
         message: 'hello from the server'
     });
 });
- 
+
 // post request 
-app.post('/data',(req, res, next)=>{
-res.status(200).json({
-message:req.body
-});
+app.post('/data', (req, res, next) => {
+    res.status(200).json({
+        message: req.body
+    });
 });
 app.listen(process.env.PORT, () => {
-    console.log(`the server isn running on port ${process.env.PORT}`)
+    console.log(`the server is running on port ${process.env.PORT}`)
 });
