71ca8588d5494376443ae4d4bb3efcadc8b49c92
INFINITYVDI007\infinitydev1 <getasewmequanint@cbe.com.et>
2022-08-10 14:17:24 +0300

    Change express.json to body-parser

--- src/index.server.js
+++ src/index.server.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const env = require('dotenv')
 const app = express();
+const bodyParser = require('body-parser')
 
 // environemt variable 
 env.config();
+// instead pog app,use(express.json())
+app.use(bodyParser());
 
 app.get('/', (req, res, next) => {
     res.status(200).json({
         message: 'hello from the server'
     });
 });
-
+ 
+// post request 
+app.post('/data',(req, res, next)=>{
+res.status(200).json({
+message:req.body
+});
+});
 app.listen(process.env.PORT, () => {
     console.log(`the server isn running on port ${process.env.PORT}`)
 });
