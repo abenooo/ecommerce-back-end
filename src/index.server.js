7195e806228dabf70e739beada7988fbbec0f485
INFINITYVDI007\infinitydev1 <getasewmequanint@cbe.com.et>
2022-08-10 12:07:44 +0300

    Test req on postman

--- src/index.server.js
+++ src/index.server.js
@@ -6,11 +6,11 @@ const app = express();
 env.config();
 
 app.get('/', (req, res, next) => {
-    req.status(200).json({
+    res.status(200).json({
         message: 'hello from the server'
-    })
-})
+    });
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`the server isn running on port ${process.env.PORT}`)
-})
+});
